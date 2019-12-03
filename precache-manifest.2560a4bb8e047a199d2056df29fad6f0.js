1871db6e432cf7f972303c69ba0563433b3aa1ff
Oles Marola <oles_pw@ukr.net>
2019-12-03 17:08:35 +0200

    Updates

---
+++ precache-manifest.2560a4bb8e047a199d2056df29fad6f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596d4121db2f4b4ea35fd4c912415249",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "594d6c23cc6a21ae3062",
+    "url": "/react_static-list-of-todos/static/css/main.c83421d8.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "594d6c23cc6a21ae3062",
+    "url": "/react_static-list-of-todos/static/js/main.0255dfd9.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.2560a4bb8e047a199d2056df29fad6f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_static-list-of-todos/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
